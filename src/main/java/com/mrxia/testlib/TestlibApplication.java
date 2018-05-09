f95bdf981ca5b73ee0ac035a5c27a5fd61dfc740
xiazijian <527091041@qq.com>
2018-05-09 23:19:44 +0800

    chore(pom): add required library

--- src/main/java/com/mrxia/testlib/TestlibApplication.java
+++ src/main/java/com/mrxia/testlib/TestlibApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * 应用启动类
+ *
+ * @author xiazijian
+ */
 @SpringBootApplication
 public class TestlibApplication {
 
