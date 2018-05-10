a9e30a94335eddd9a72dde74893a3a82d7716277
夏子健 <xiazijian666@gmail.com>
2018-05-10 18:54:27 +0800

    feat(subject): add Subject class and TestPaper class

---
+++ src/main/java/com/mrxia/testlib/beans/data/Subject.java
@@ -0,0 +1,10 @@
+package com.mrxia.testlib.beans.data;
+
+/**
+ * 试题科目类
+ * @author xiazijian
+ */
+public class Subject {
+
+    
+}
---
+++ src/main/java/com/mrxia/testlib/beans/data/TestPaper.java
@@ -0,0 +1,8 @@
+package com.mrxia.testlib.beans.data;
+
+/**
+ * 试题试卷类
+ * @author xiazijian
+ */
+public class TestPaper {
+}
