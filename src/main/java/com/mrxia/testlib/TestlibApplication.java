bde8c379278f40fcd508d3f7b49ef1e270282411
xiazijian <527091041@qq.com>
2018-06-21 23:56:19 +0800

    chore(config): change converter contentType setting

--- src/main/java/com/mrxia/testlib/TestlibApplication.java
+++ src/main/java/com/mrxia/testlib/TestlibApplication.java
@@ -1,19 +1,11 @@
 package com.mrxia.testlib;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
@@ -35,15 +27,7 @@ public class TestlibApplication {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        RestTemplate template = builder.build();
-        /*for (HttpMessageConverter<?> converter : template.getMessageConverters()) {
-            if (converter instanceof MappingJackson2HttpMessageConverter) {
-                MappingJackson2HttpMessageConverter c = (MappingJackson2HttpMessageConverter) converter;
-               new ArrayList<>((MappingJackson2HttpMessageConverter) converter).getSupportedMediaTypes())
-                        .setSupportedMediaTypes(Collections.singletonList(MediaType.TEXT_HTML));
-            }
-        }*/
-        return template;
+        return builder.build();
     }
 
     @Bean
--- src/main/java/com/mrxia/testlib/config/TestLibWebMvcConfigurerAdapter.java
+++ src/main/java/com/mrxia/testlib/config/TestLibWebMvcConfigurerAdapter.java
@@ -1,7 +1,14 @@
 package com.mrxia.testlib.config;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,5 +34,22 @@ public class TestLibWebMvcConfigurerAdapter implements WebMvcConfigurer {
         registry.addInterceptor(new UserInterceptor(loginPath))
                 .addPathPatterns("/**")
                 .excludePathPatterns(loginPath, staticPath, "/error");
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        for (HttpMessageConverter<?> converter : converters) {
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+
+                // 先将原先支持的MediaType列表拷出
+                List<MediaType> mediaTypeList = new ArrayList<>(converter.getSupportedMediaTypes());
+
+                // 加入对text/html的支持
+                mediaTypeList.add(MediaType.TEXT_HTML);
+
+                // 将已经加入了text/html的MediaType支持列表设置为其支持的媒体类型列表
+                ((MappingJackson2HttpMessageConverter) converter).setSupportedMediaTypes(mediaTypeList);
+            }
+        }
     }
 }
