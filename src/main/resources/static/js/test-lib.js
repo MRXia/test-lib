cf67ffcf242c41ec277d72d064b3e88ce8e8832d
xiazijian <527091041@qq.com>
2018-06-25 00:00:14 +0800

    feat(test lib): implement checked answer function
    - change request from "fetch" to "ajax"
    - upgrade art-template lib
    - implement the answer check function

--- src/main/resources/static/js/test-lib.js
+++ src/main/resources/static/js/test-lib.js
@@ -1,35 +1,29 @@
 const apiRequest = function (url, data, method) {
-    let isOk = false;
-    return fetch(url, {
+    return jQuery.ajax(url, {
         method: method || 'GET',
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-            'credentials': 'include',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+        contentType: 'application/json',
+        data: JSON.stringify(data)
     }).then(function (response) {
-        isOk = response.ok;
-        return response.json();
-    }).then(function (data) {
-        if (isOk) {
-            return data;
+        if (response.code === 200) {
+            return response.data;
         }
-        alert.error(data.msg);
-        return Promise.reject(data.msg);
+        Alert.error(response.msg);
+        return Promise.reject(response.msg);
     });
 };
 
 const testPaper = (function ($) {
 
-    let paper,
+    let paper = {
+            questions:[]
+        },
         stat = {
             current : 0,    // 当前题号
             score : 0,      // 总分
             selected : [],  // 已选情况
         },
-        question = $("#questionContent");
+        question = $("#questionContent"),
+        checked = $('<span class="oi oi-check"></span>');
 
     let init = function (subjectId, paperId) {
         apiRequest("/paper/start",
@@ -44,7 +38,7 @@ const testPaper = (function ($) {
                 countdown(data.testTime)
             })
             .then(function () {
-
+                selectQuestion(0);
             });
         },
 
@@ -55,24 +49,39 @@ const testPaper = (function ($) {
         // 选择答案
         chooseAnswer = function (answer) {
             stat.selected[stat.current] = answer;
+            checkedOption(answer);
         },
 
+        // 选择考题
         selectQuestion = function (qid) {
             stat.current = qid;
             question.html(template("questionTpl", {
                 question: paper.questions[qid],
-                current: qid
+                current: qid,
+                isLast: qid === paper.questions.length - 1
             }));
+
+            let answer = stat.selected[qid];
+            if (answer) {
+                checkedOption(answer);
+            }
+        },
+
+        checkedOption = function (answer) {
+            question.find(".list-group .list-group-item").children().remove();
+            $("#answer" + answer).append(checked);
         }
     ;
 
     return {
-        "init": init
+        "init": init,
+        "selectQuestion": selectQuestion,
+        "chooseAnswer": chooseAnswer
     }
 
 })(jQuery);
 
-const alert = (function ($) {
+const Alert = (function ($) {
 
     let showMessage = function (data) {
 
