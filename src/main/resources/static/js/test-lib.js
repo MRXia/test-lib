e8176bbcdde308f65c00d8dba0e601609c25aaec
xiazijian <527091041@qq.com>
2018-07-05 00:43:58 +0800

    feat(question paper): implement operate question select

--- src/main/resources/static/js/test-lib.js
+++ src/main/resources/static/js/test-lib.js
@@ -20,12 +20,15 @@ const testPaper = (function ($) {
     class QuestionType {
         constructor(tplId) {
             this.tplId = tplId;
+            this.answerArray = ['A', 'B', 'C', 'D'];
         }
 
+        // 构建题目数据
         createData(question) {
             return question;
         }
 
+        // 使用题目数据和题号，调用对应模板，构建题目
         createQuestion(question, index) {
             return template(this.tplId, {
                 question:this.createData(question),
@@ -34,9 +37,10 @@ const testPaper = (function ($) {
             });
         }
 
-        chooseAnswer(answer) {
-            this.checkedAnswer(answer);
-            this.checkedOption(answer);
+        // 选择答案操作
+        chooseAnswer(answer, item) {
+            this.checkedAnswer(answer, item);
+            this.checkedOption(answer, item);
         }
 
         // 选择答案赋值操作
@@ -47,12 +51,27 @@ const testPaper = (function ($) {
         // 选择答案视图操作
         checkedOption(answer) {
         }
+
+        /* 多选题选择答案时，根据答案列表与当前选项，返回选择后的结果
+         * 遍历A,B,C,D选项，如果当前答案包含而被选中，则去除选项，如果当前答案未包含而选中，则添加该选项
+         * 如已选择"AC", 若选择"B"选项，则返回"ABC";若选择"A"选项，则返回"C"
+         */
+        checkedMultiAnswer(answer, currentAnswer) {
+            return this.answerArray.map(value => {
+                let choose = value === answer;
+                if (currentAnswer.indexOf(value) === -1) {
+                    return choose ? value : '';
+                } else {
+                    return choose ? '' : value;
+                }
+            }).join('');
+        }
     }
 
     /*单选题类型*/
     class SingleChoiceQuestion extends QuestionType {
         constructor() {
-            super("singleChoiceTpl")
+            super("singleChoiceTpl");
         }
 
         checkedOption(answer) {
@@ -64,23 +83,11 @@ const testPaper = (function ($) {
     class MultipleChoiceQuestion extends QuestionType {
         constructor() {
             super("multipleChoiceTpl");
-            this.answerArray = ['A', 'B', 'C', 'D'];
         }
 
         checkedAnswer(answer) {
             let currentAnswer = stat.selected[stat.current] || '';
-
-            // 遍历A,B,C,D选项，如果当前答案包含而被选中，则去除选项，如果当前答案未包含而选中，则添加该选项
-            currentAnswer = this.answerArray.map(value => {
-                let choose = value === answer;
-                if (currentAnswer.indexOf(value) === -1) {
-                    return choose ? value : '';
-                } else {
-                    return choose ? '' : value;
-                }
-            }).join('');
-
-            stat.selected[stat.current] = currentAnswer;
+            stat.selected[stat.current] = this.checkedMultiAnswer(answer, currentAnswer);
         }
 
         checkedOption(answer) {
@@ -96,7 +103,7 @@ const testPaper = (function ($) {
     /*判断题类型*/
     class TrueOrFalseQuestion extends QuestionType {
         constructor() {
-            super("trueFalseTpl")
+            super("trueFalseTpl");
         }
 
         checkedOption(answer) {
@@ -107,7 +114,108 @@ const testPaper = (function ($) {
     /*操作题类型*/
     class OperateQuestion extends QuestionType {
         constructor() {
-            super("operateTpl")
+            super("operateTpl");
+        }
+
+        createData(question) {
+
+            // 如果操作题未被渲染，则进行预处理
+            if (!question.items) {
+                this.renderItems(question);
+            }
+
+            return question;
+        }
+
+        renderItems(question){
+
+            let title = "";
+            let items = [];
+            let item;
+
+            // 通过<br>切割操作题问题，进行预处理
+            let itemStart = false;
+            let itemStatus;
+
+            question.title.split("<br>").forEach(value => {
+
+                // 排除字符串为空的行
+                if(!$.trim(value)) {
+                    return;
+                }
+
+                // 判断小题是否开始, 如果未开始，则为大题目，拼接进title
+                itemStart = itemStart || /第1小题\./.test(value);
+                if (!itemStart) {
+                    title += value + "<br>";
+                    return;
+                }
+
+                // 处理单个选项和题目不止一行的情况，通过起始字符变换状态
+                if (/第\d小题\./.test(value)) {
+                    if (item) {
+                        items.push(item);
+                    }
+                    item = {title:''};
+                    itemStatus = 'title';
+                } else {
+                    switch ($.trim(value).substr(0, 1)) {
+                        case 'A':
+                            item.answerA = value;
+                            itemStatus = 'answerA';
+                            break;
+                        case 'B':
+                            item.answerB = value;
+                            itemStatus = 'answerB';
+                            break;
+                        case 'C':
+                            item.answerC = value;
+                            itemStatus = 'answerC';
+                            break;
+                        case 'D':
+                            item.answerD = value;
+                            itemStatus = 'answerD';
+                            break;
+                        default:
+                            item[itemStatus] += value;
+                    }
+                }
+            });
+
+            // 放入最后一题
+            if (item) {
+                items.push(item);
+            }
+
+            question.title = title;
+            question.items = items;
+        }
+
+        checkedAnswer(answer, item) {
+
+            if (!stat.selected[stat.current]) {
+                stat.selected[stat.current] = [];
+            }
+
+            let currentAnswer = stat.selected[stat.current][item] || '';
+            stat.selected[stat.current][item] = this.checkedMultiAnswer(answer, currentAnswer);
+        }
+
+        checkedOption(answer, item) {
+            if(typeof(answer) === "string" && typeof(item) !== "undefined") {
+                this.checkedItem(item);
+            } else {
+                answer.forEach((value, index) => {
+                    this.checkedItem(index);
+                })
+            }
+        }
+
+        checkedItem(item) {
+            question.find(".list-group#item" + item + " .list-group-item").children().remove();
+            stat.selected[stat.current][item].split('').forEach(value => {
+                $("#item" + item + "-answer" + value).append(checked.clone());
+            });
         }
     }
 
@@ -155,8 +263,10 @@ const testPaper = (function ($) {
         },
 
         // 选择答案
-        chooseAnswer = function (answer) {
-            questionTypeEnum[paper.questions[stat.current].type].chooseAnswer(answer);
+        chooseAnswer = function () {
+            let args = Array.prototype.slice.call(arguments);
+            let typeEnum = questionTypeEnum[paper.questions[stat.current].type];
+            typeEnum.chooseAnswer.apply(typeEnum, args);
         },
 
         // 选择考题
